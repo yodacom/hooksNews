f6cf6645b2dcda8188ed05b1935fba0c65b9b8c3
Jeremy Black <jb@yodacom.com>
2019-07-25 09:31:30 -0600

    added submit links fuctionality

--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useFormValidation from '../Auth/useFormValidation';
 import validateCreateLink from '../Auth/validateCreateLink';
+import { FirebaseContext } from "../../firebase";
 
 const INITIAL_STATE = {
   description: '',
@@ -8,10 +9,30 @@ const INITIAL_STATE = {
 }
 
 function CreateLink(props) {
+  const { firebase, user } = React.useContext(FirebaseContext)
+  console.log({user})
+
   const { handleSubmit, handleChange, values, errors } = useFormValidation(INITIAL_STATE, validateCreateLink, handleCreateLink)
   
   function handleCreateLink() {
-    console.log("Link Created!")
+    if (!user) {
+      props.history.push('/login')
+    } else {
+      const { url, description } = values
+      const newLink = {
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName
+        },
+        votes: [],
+        comments: [],
+        created: Date.now()
+      }
+      firebase.db.collection('links').add(newLink)
+      props.history.push("/");
+    }
   }
     return (
       <form onSubmit={handleSubmit} className="flex flex-column mt3">
@@ -31,7 +52,7 @@ function CreateLink(props) {
           name="url"
           placeholder="The URL for the link"
           autoComplete="off"
-          type="text"
+          type="url"
           className={errors.url && 'error-input'}
           />
           {errors.url && <p className="error-text">{errors.url}</p>}
--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import FirebaseContext from '../../firebase/context';
 
 function LinkList(props) {
   return <div>LinkList</div>;
